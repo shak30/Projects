0d4e5d45bc00d2507459ddd8b6742ed0b011c4c5
shak30 <shakti3002@gmail.com>
2022-11-09 16:06:57 +0530

    added capital gain and loss

--- Classification_project/Classification.py
+++ Classification_project/Classification.py
@@ -38,9 +38,9 @@ missing=data[data.isnull().any(axis=1)]
 
 data2=data.dropna(axis=0)
 
-#===============================
+#======================================
 #Cheking correlation between variables
-#===============================
+#======================================
 
 #checking correlation between independent variables
 correlation= data2.corr()
@@ -91,6 +91,18 @@ occupation_vs_salstat=pd.crosstab(index=data2['occupation'],
 
 #hoursperweek vs salarystatus
 hoursperweek=sns.boxplot('hoursperweek','SalStat',data=data2)
+
+#capital gain and loss
+sns.distplot(data2['capitalgain'],bins=10,kde=False)
+
+sns.distplot(data2['capitalloss'],bins=10,kde=False)
+
+
+#=======================================================
+       #LOGISTIC REGRESSION
+#=======================================================
+
+
 
 
 
