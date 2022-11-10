5934b07532c23b3dc5b6df6721f8e1d7754234d5
shak30 <shakti3002@gmail.com>
2022-11-10 19:50:32 +0530

    small changes in logistic regresssion model with reduced no of variables

--- Classification_project/Classification.py
+++ Classification_project/Classification.py
@@ -161,60 +161,71 @@ print('Misclassified samples: %d' % (test_y != prediction).sum())
   #LOGISTIC REGRESSION - REMOVING INSIGNIFICANT VARIABLES
 #=============================================================================
   
-# # reindexing the salary status names to 0,1
-# # data2['SalStat']=data2['SalStat'].map({' less than or equal to 50,000':0,' greater than 50,000':1})
-# print(data2['SalStat'])
+# reindexing the salary status names to 0,1
+# data2['SalStat']=data2['SalStat'].map({' less than or equal to 50,000':0,' greater than 50,000':1})
+print(data2['SalStat'])
 
 
-# cols=['gender','nativecountry','race','JobType']
-# data2= data2.drop(cols,axis=1)
+cols=['gender','nativecountry','race','JobType']
+data2= data2.drop(cols,axis=1)
       
-# new_data1=pd.get_dummies(data2, drop_first=True)
+new_data1=pd.get_dummies(data2, drop_first=True)
 
-# #Storing the column names 
-# column_list=list(new_data1.columns)
-# print(column_list)
+#Storing the column names 
+column_list=list(new_data1.columns)
+print(column_list)
 
-# #Seperating the input names from data
-# features=list(set(column_list)-set(['SalStat']))
-# print(features)
+#Seperating the input names from data
+features=list(set(column_list)-set(['SalStat']))
+print(features)
 
-# #storing the column values in Y
-# y1=new_data1['SalStat'].values
-# print(y)
+#storing the column values in Y
+y1=new_data1['SalStat'].values
+print(y)
 
-# #storing the input values from features in X
-# x1=new_data1[features].values
-# print(x)
+#storing the input values from features in X
+x1=new_data1[features].values
+print(x)
 
-# #splitting the data into train and test
+#splitting the data into train and test
 
-# train_x1,test_x1,train_y1,test_y1=train_test_split(x1,y1,test_size=0.3,random_state=0)
+train_x1,test_x1,train_y1,test_y1=train_test_split(x1,y1,test_size=0.3,random_state=0)
 
-# #make an instance of the model
-# logistic1=LogisticRegression()
+#make an instance of the model
+logistic1=LogisticRegression()
 
-# # fitting the values for x and y
-# logistic1.fit(train_x1,train_y1)
-# logistic1.coef_
-# logistic1.intercept_
+# fitting the values for x and y
+logistic1.fit(train_x1,train_y1)
+logistic1.coef_
+logistic1.intercept_
 
 
-# # prediction from test data
-# prediction = logistic1.predict(test_x1)
+# prediction from test data
+prediction = logistic1.predict(test_x1)
 
-# from sklearn.metrics import confusion_matrix
-# from sklearn.metrics import accuracy_score
-# #confusion matrix
-# confusion_matrix1= confusion_matrix(test_y1, prediction)
-# print(confusion_matrix1)
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+#confusion matrix
+confusion_matrix1= confusion_matrix(test_y1, prediction)
+print(confusion_matrix1)
 
-# #calculating the accuracy
-# accuracy_score1= accuracy_score(test_y1, prediction)
-# print(accuracy_score1)
+#calculating the accuracy
+accuracy_score1= accuracy_score(test_y1, prediction)
+print(accuracy_score1)
 
-# #Printing the misclassified values from prediction
-# print('Misclassified samples: %d' % (test_y != prediction).sum())
+#Printing the misclassified values from prediction
+print('Misclassified samples: %d' % (test_y != prediction).sum())
+
+#------------------------------------------------------------------------
+#Accuracy of reduced no of insignificant variables compromised very less.
+#so, we can consider the solution with less no of variables.
+#------------------------------------------------------------------------
+
+
+#=========================================================================
+            #KNN REGRESSION MODEL
+#=========================================================================
+
 
 
 
